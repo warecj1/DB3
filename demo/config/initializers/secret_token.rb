fa0dedad2b8269e6b97df351ab02f719ae92a240
Chris <warecj1@student.op.ac.nz>
2014-07-30 02:30:59 +0000

    initial commit

---
+++ demo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = 'c036f9a7a262d4a78da600b4f81eb352fbd57ad22ffb33f9fef82fb00fc2e50dc2d4897a73ee95c15939d230d3f0eff597bb6a7dd1ba573707efed6ca820a894'
---
+++ splatter/db/migrate/20140730013824_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password
+      t.string :name
+      t.text :blurb
+
+      t.timestamps
+    end
+  end
+end
